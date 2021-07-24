9f1d3359f6eb9e8ec1ab65a1c4b16b1f3d429015
pakawaka1 <davidcervantesdev@gmail.com>
2021-07-24 10:57:20 -0700

    feat(server.js): created server.js file and introduced dependencies

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+
+// Route Files
+const movies = require('./routes/movies');
+const reviews = require('./routes/reviews');
+
+const app = express();
+
+const corsOptions = {
+  origin: 'http://localhost:5001',
+};
+
+app.use(cors(corsOptions));
+
+// Parse incomings data requests
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Db Models
+const db = require('./models');
+if (process.nev.NODE_ENV === 'development') {
+    db.sequelize.sync({ force: true }).then(() => {
+        console.log("Drop and re-sync db.");
+      });
+} else {
+    db.sequelize.sync();
+};
+
+app.use('/api/v1/movies', movies);
+app.use('/api/v1/reviews' reviews);
+
+// port settings.
+const PORT = process.env.PORT || 5000;
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
