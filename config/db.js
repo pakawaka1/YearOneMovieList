0382602bbfca2a939499e884f4ef071c9efed3af
pakawaka1 <davidcervantesdev@gmail.com>
2021-08-19 00:52:23 -0700

    fix(controllers): merging with dev with updated code styling

--- config/db.js
+++ config/db.js
@@ -1,14 +1,19 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize('postgres', 'postgres', 'Slayer4949', {
-  host: 'localhost',
-  dialect: 'postgres',
-  operatorsAliases: 0,
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-});
+const db = new Sequelize(
+  'moviereviews',
+  'postgres',
+  'Your PostgreSQL Password Goes Here',
+  {
+    host: 'localhost',
+    dialect: 'postgres',
+    operatorsAliases: 0,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  }
+);
 
 module.exports = db;
