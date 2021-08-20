1f76dee72a19d95c8bc668af465f6367522151e7
pakawaka1 <davidcervantesdev@gmail.com>
2021-08-20 01:05:00 -0700

    refactor(addNewReview): now able to access title from reviews form

--- controllers/add.js
+++ controllers/add.js
@@ -4,14 +4,13 @@ const Review = require('../models/Review');
 // add new review to database
 exports.addNewReview = async (req, res) => {
   try {
-    const { imdbID } = req.query;
-    const movieData = await movie.getOneMovie(imdbID);
+    const title = Object.keys(req.body).toString();
     const [review, created] = await Review.findOrCreate({
-      where: { title: movieData.Title },
+      where: { title },
       defaults: { thumbsUp: 0, thumbsDown: 0 },
     });
-    if (req.body.review === 'thumbsUp') review.thumbsUp++;
-    if (req.body.review === 'thumbsDown') review.thumbsDown++;
+    if (req.body[title] === 'thumbsUp') review.thumbsUp++;
+    if (req.body[title] === 'thumbsDown') review.thumbsDown++;
     await review.save();
     res.redirect('back');
   } catch (err) {
