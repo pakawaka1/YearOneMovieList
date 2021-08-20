6c3131aa5fd7c5f9b272624f96652eb3d7a172c7
pakawaka1 <davidcervantesdev@gmail.com>
2021-08-20 00:08:47 -0700

    refactor(addNewReview): updated how to access movie title

--- config/db.js
+++ config/db.js
@@ -1,13 +1,18 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize('moviereviews', 'postgres', 'Slayer4949', {
-  host: 'localhost',
-  dialect: 'postgres',
-  operatorsAliases: 0,
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-});
+const db = new Sequelize(
+  'moviereviews',
+  'postgres',
+  'Your PostgreSQL Password Goes Here',
+  {
+    host: 'localhost',
+    dialect: 'postgres',
+    operatorsAliases: 0,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  }
+);
 module.exports = db;
